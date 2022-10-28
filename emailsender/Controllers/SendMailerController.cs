aea08034fce42c801a672b10c28abf1ab325b89b
mateusz <mateuszbronclik@gmail.com>
2022-10-28 15:26:14 +0200

    working on landing page for app

--- emailsender/Controllers/SendMailerController.cs
+++ emailsender/Controllers/SendMailerController.cs
@@ -13,13 +13,29 @@ namespace emailsender.Controllers
         }
 
         [HttpPost]
-        public ViewResult Index(emailsender.Models.MailModel _objModelMail)
+        public ViewResult Index(Models.MailModel _objModelMail)
         {
             if (ModelState.IsValid)
             {
                 MailMessage mail = new MailMessage();
                 mail.To.Add(_objModelMail.To);
                 mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = new System.Net.NetworkCredential("username", "password"); // podac haslo i email wysylajacego
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Index", _objModelMail);
+            }
+            else
+            {
+                return View();
             }
         }
 
--- emailsender/Models/MailModel.cs
+++ emailsender/Models/MailModel.cs
@@ -1,11 +1,16 @@
-﻿namespace emailsender.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace emailsender.Models
 {
     public class MailModel
     {
+        [Required]
         public string From { get; set; }
-       
+
+        [Required]
         public string To { get; set; }
-      
+
+        [Required]
         public string Subject { get; set; }
       
         public string Body { get; set; }
