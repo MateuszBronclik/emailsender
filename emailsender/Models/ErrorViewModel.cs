5d576148d5e08f9d9008c61fc0fce7f110e1adde
mateusz <mateuszbronclik@gmail.com>
2022-10-28 11:52:08 +0200

    added project

---
+++ emailsender/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace emailsender.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
