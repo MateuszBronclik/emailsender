79020713db3dfd9fe0bde0cd6f1c33a2941bb68d
mateusz <mateuszbronclik@gmail.com>
2022-10-30 17:54:14 +0100

    changed size of window

--- emailsender/Models/MailModel.cs
+++ emailsender/Models/MailModel.cs
@@ -5,16 +5,16 @@ namespace emailsender.Models
     public class MailModel
     {
         [Required]
-        public string? From { get; set; }
+        public string From { get; set; }
 
         [Required]
-        public string? To { get; set; }
+        public string To { get; set; }
 
         [Required]
         [StringLength(256, ErrorMessage = "The Subject value cannot exceed 256 characters. ")]
-        public string? Subject { get; set; }
+        public string Subject { get; set; }
         
-        public string? Body { get; set; }
+        public string Body { get; set; }
        
     }
 }
